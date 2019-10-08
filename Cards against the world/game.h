b6d71abf1d1d7d3fbd7f5f2f82b947448515f562
sylwow <Sylwo1003@gmail.com>
2019-10-08 15:28:23 +0200

    Update game.h

--- Cards against the world/game.h
+++ Cards against the world/game.h
@@ -4,19 +4,19 @@
 #include "socketUtils.h"
 #define LEN 4000
 
-
 namespace message {
-	const char unKnownError = 0;
-	const char lobbyIsAlredyChosen = 1;
-	const char noPortsAreAvailable = 2
-	const char lobbyNameIsIncorrect = 3;
-	const char couldNotFindLobby = 4;
-	const char ServerIsFull = 5;
-	const char lobbyNameIsLocked = 6;//server end
-	const char unableToRechServer = 7;
-	const char unableToSendData = 8;
-	const char unableToGetData = 9;
-	const char connected = 10;
+	typedef char code;
+	const code unKnownError = 0;
+	const code lobbyIsAlredyChosen = 1;
+	const code noPortsAreAvailable = 2
+	const code lobbyNameIsIncorrect = 3;
+	const code couldNotFindLobby = 4;
+	const code ServerIsFull = 5;
+	const code lobbyNameIsLocked = 6;//server end
+	const code unableToRechServer = 7;
+	const code unableToSendData = 8;
+	const code unableToGetData = 9;
+	const code connected = 10;
 	namespace {//private
 	const std::vector<std::string> connectionError { 
 		"Unknown error", 
@@ -27,9 +27,9 @@ namespace message {
 		"Server is full",
 		"Lobby name is locked"  };//7
 	}
-	std::string getConnectError(char error) {
+	std::string getConnectError(code error) {
 	if(error < 1 || error >= errorVector.size())
-	return errorVector[0];
+		return errorVector[0];
 	return errorVector[error];
 	}
 }
@@ -52,7 +52,7 @@ private:
 	char playerId;
 public:
 	game(sf::RenderWindow& win, sf::String lobbyId, sf::String nick, bool newlobby);
-	ConnectErrors connect();
+	message::code connect();
 	bool Send(std::u32string s, sf::TcpSocket& socket);
 	bool receive(sf::TcpSocket& socket, std::u32string& data, char& coding, char& playerId);
 	~game();
