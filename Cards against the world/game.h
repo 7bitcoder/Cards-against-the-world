b8f85128719b17e25e674a5dfe4b446e537ff784
sylwow <Sylwo1003@gmail.com>
2019-10-24 11:50:29 +0200

    Update game.h

--- Cards against the world/game.h
+++ Cards against the world/game.h
@@ -1,14 +1,64 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+#include<algorithms>
 #include "socketUtils.h"
 #include"messages.h"
 #include "chat.h"
 #define LEN 4000
-
+struct data {
+int id
+}
+class Cards{//front is top back is bottom of cards deque
+	private:
+	std::deque<data> cards;
+	public:
+	deques();{}
+	void init(int wh )
+	{
+	cards.clear();
+	for(int i = 0; i<wh; i++)
+		cards.push_back(i);
+	}
+	void shuffle() {
+	std::random_shuffle(cards.begin(), cards.end());
+	}
+	int getCard() { 
+	int id = cards.front().id; 
+	cards.pop_front();
+	return id;
+	}
+	bool empty() { return cards.empty();}// do sprawdzenia czy talia jest pusta czarna przy rozpoczecie rundy
+	void putCardsBack(std::vector<int> & car){
+	for(int x : car)
+		cards.push_back(x);
+	}
+};
+class whiteCards:public Cards{
+	private:
+	std::set<int> cardsTaken;//check if putCards back are OK
+	public:
+	int getCard(){
+	int  id = Cads::getCard();
+	cardsTaken.insert(id);
+	return id;
+	}
+	bool putCardsBack(std::vector<int> & car){
+	for(int x : car){
+		auto it = cardsTaken.find(x);
+		if(it == cardsTaken.end()
+		   return false;
+		cardsTaken.erase(it);
+	}
+	Cards::putCardsBack(car);
+	return true;		
+	}
+};
 class game :public socketUtils
 {
 private:
+	Cards black;
+	whiteCards white;
 	enum state{lobby, GameBegin, exit};
 	state state_;
 	chat Chat;
@@ -26,6 +76,7 @@ private:
 	bool newLobby;
 	char playerId;
 	bool ready_ = false;
+	bool lockLobby  = false;
 	//
 	sf::Texture backgroundTexture;
 	sf::Sprite background;
