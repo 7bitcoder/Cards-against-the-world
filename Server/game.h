210e2135c3f97d1705227523c7ca49783cf986f1
sylwow <Sylwo1003@gmail.com>
2019-10-24 11:55:15 +0200

    Update game.h

--- Server/game.h
+++ Server/game.h
@@ -3,6 +3,51 @@
 #include "serverUtils.h"
 #define LEN 8000
 enum states{ waiting = 1, starting, questionInit, question, choseInit, chose, summing, kill};
+class Cards{//front is top back is bottom of cards deque
+	private:
+	std::deque<data> cards;
+	public:
+	deques();{}
+	void init(int wh )
+	{
+	cards.clear();
+	for(int i = 0; i<wh; i++)
+		cards.push_back(i);
+	}
+	void shuffle() {
+	std::random_shuffle(cards.begin(), cards.end());
+	}
+	int getCard() { 
+	int id = cards.front().id; 
+	cards.pop_front();
+	return id;
+	}
+	bool empty() { return cards.empty();}// do sprawdzenia czy talia jest pusta czarna przy rozpoczecie rundy
+	void putCardsBack(std::vector<int> & car){
+	for(int x : car)
+		cards.push_back(x);
+	}
+};
+class whiteCards:public Cards{
+	private:
+	std::set<int> cardsTaken;//check if putCards back are OK
+	public:
+	int getCard(){
+	int  id = Cads::getCard();
+	cardsTaken.insert(id);
+	return id;
+	}
+	bool putCardsBack(std::vector<int> & car){
+	for(int x : car){
+		auto it = cardsTaken.find(x);
+		if(it == cardsTaken.end()
+		   return false;
+		cardsTaken.erase(it);
+	}
+	Cards::putCardsBack(car);
+	return true;		
+	}
+};
 struct slot
 {
 	std::u32string nick;
@@ -12,6 +57,8 @@ struct slot
 class game: public serverUtils
 {
 private:
+	Cards black;
+	whiteCards white;
 	bool lock;//lock lobby for new players
 	char buff[LEN];
 	char free[9];//free table to check if slot of id is free;
